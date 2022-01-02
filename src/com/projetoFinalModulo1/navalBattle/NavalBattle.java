da8f90211a65ee04eb8721796f0d81d5f457eeec
Felipe Luiz <felipe.andrade33@etec.sp.gov.br>
2022-01-02 17:53:11 -0300

    Classe Board: Realizada retificações nos métodos. Classe navalBattle: inicio do método start, Classes Human, Bot e Player: criado metodo setName e retificado atributos.

--- src/com/projetoFinalModulo1/navalBattle/NavalBattle.java
+++ src/com/projetoFinalModulo1/navalBattle/NavalBattle.java
@@ -1,16 +1,43 @@
 package com.projetoFinalModulo1.navalBattle;
 
-import com.projetoFinalModulo1.navalBattle.auxClass.Board;
+import com.projetoFinalModulo1.navalBattle.auxClass.Bot;
+import com.projetoFinalModulo1.navalBattle.auxClass.Human;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class NavalBattle {
 
 
 public static void start(){
-    Board board = new Board();
-    board.setInitialPositions();
-    board.printPosition();
-    board.updatePositions(1, 'c', "X");
-    board.printPosition();
+    Scanner sc = new Scanner(System.in);
+
+    Bot bot = new Bot();
+    bot.setName();
+    bot.board.setEmptyPositions();
+    bot.board.setShips(true);
+//    bot.board.printPosition();
+
+    Human human = new Human();
+    System.out.print("Digite seu nome: ");
+    String name = sc.nextLine();
+    human.setName(name);
+//    System.out.println(human.getName());
+    boolean invalidOptSetShips = true;
+    boolean setShipsAuto = true;
+    while(invalidOptSetShips){
+        System.out.print("Escolha uma das opções para espalhar seus navios pelo tabuleiro(1) para automático ou (2) manual: ");
+        int opt = Integer.parseInt(sc.nextLine());
+        if(opt == 1 || opt == 2){
+            invalidOptSetShips = false;
+            if (opt == 2){
+                setShipsAuto = false;
+            }
+        }
+    }
+    human.board.setEmptyPositions();
+    human.board.setShips(setShipsAuto);
+    human.board.printPosition();
 }
     private boolean verifyWinner() {
         return false;
--- src/com/projetoFinalModulo1/navalBattle/auxClass/Board.java
+++ src/com/projetoFinalModulo1/navalBattle/auxClass/Board.java
@@ -1,30 +1,68 @@
 package com.projetoFinalModulo1.navalBattle.auxClass;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Board {
     private final String[][] POSITIONS = new String[10][10];
     private int ships = 10;
 
-    public void updatePositions(int column, char rowLetter, String mark) {
-        int row = this.getNumber(rowLetter);
-            POSITIONS[row][column] = mark;
+//    public void updatePositions(int column, char rowLetter, String mark) {
+//        int row = this.getNumber(rowLetter);
+//            this.POSITIONS[row][column] = mark;
+//    }
+    public void updatePositions(int column, int row, String mark) {
+        this.POSITIONS[row][column] = mark;
     }
     public void printPosition() {
         System.out.printf("|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |%n");
-        for (int i = 0; i < POSITIONS.length; i++) {
-            System.out.printf("| %s | %s |%n" ,this.getLetter(i), String.join(" | ", POSITIONS[i]));
+        for (int i = 0; i < this.POSITIONS.length; i++) {
+            System.out.printf("| %s | %s |%n" ,this.getLetter(i), String.join(" | ", this.POSITIONS[i]));
         }
     }
-//    fazer a inclusão dos navios com o metodo updatePositions
-//    public void setShips(int column, char rowLetter) {
-//        this.updatePositions(column, rowLetter, "n");
-//    }
+
+    public boolean isValidPosition(int column, int row) {
+        return Objects.equals(this.POSITIONS[row][column], " ");
+    }
+
+    //    fazer a inclusão dos navios com o metodo updatePositions
+    public void setShips(boolean isAuto) {
+        int i = 0;
+//        int column;
+//        int row;
+        while (i < this.ships) {
+                Random random = new Random();
+                int column = random.nextInt(10);
+                int row = random.nextInt(10);
+            if(!isAuto){
+                Scanner sc = new Scanner(System.in);
+                this.printPosition();
+                System.out.printf("Para posicionar o navio %d escolha uma das opções: %nInforme uma posição ainda não preenchida em formato válido(Ex: A2, b4, c7...)%n Digite r para random%nopç: ", i+1);
+                String position = sc.nextLine();
+                if(!Objects.equals(position, "r") && !Objects.equals(position, "R")){
+                    String[] splitedPosition = position.split("");
+                    row = this.getNumber(splitedPosition[0]);
+                    column =  Integer.parseInt(splitedPosition[1]);
+                }
+            }
+//            System.out.printf("row: %d, column: %d  - %s%n", row, column, this.isValidPosition(column, row));
+            if (row != 99 && this.isValidPosition( column, row)) {
+                this.updatePositions(column, row, "N");
+                i++;
+            }
+        }
+    }
 //    Movido para NavalBattle
 //    public boolean verifyWinner() {
 //        return false;
 //    }
-
+public void setEmptyPositions(){
+    for (String[] position : POSITIONS) {
+        Arrays.fill(position, " ");
+    }
+}
     private String getLetter(int index){
         switch (index){
             case 0:
@@ -52,47 +90,43 @@ public class Board {
         }
     }
 
-    private int getNumber(char index){
+    private int getNumber(String index){
         switch (index){
-            case 'A':
-            case 'a':
+            case "A":
+            case "a":
                 return 0;
-            case 'B':
-            case 'b':
+            case "B":
+            case "b":
                 return 1;
-            case 'C':
-            case 'c':
+            case "C":
+            case "c":
                 return 2;
-            case 'D':
-            case 'd':
+            case "D":
+            case "d":
                 return 3;
-            case 'E':
-            case 'e':
+            case "E":
+            case "e":
                 return 4;
-            case 'F':
-            case 'f':
+            case "F":
+            case "f":
                 return 5;
-            case 'G':
-            case 'g':
+            case "G":
+            case "g":
                 return 6;
-            case 'H':
-            case 'h':
+            case "H":
+            case "h":
                 return 7;
-            case 'I':
-            case 'i':
+            case "I":
+            case "i":
                 return 8;
-            case 'J':
-            case 'j':
+            case "J":
+            case "j":
                 return 9;
             default:
                 return 99;
         }
     }
 
-    public void setInitialPositions(){
-        for (String[] position : POSITIONS) {
-            Arrays.fill(position, " ");
-        }
-    }
+
 }
 
